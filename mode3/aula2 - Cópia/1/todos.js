9c9685f86a3c770cf2dab398edabd6ac4ce24903
unknown <camilla_k_lima@saojose.sc.senai.br>
2019-06-17 12:25:08 -0300

    aula 2 modo3

--- mode3/aula2 - Cópia/1/todos.js
+++ mode3/aula2 - Cópia/1/todos.js
@@ -1,1 +1,14 @@
-alert('func')
+
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app");
+
+var todos = [
+    'fazer cafe',
+    'beber agua',
+    'jogar lixo no lixo'
+];
+
+console.log(listElement)
+
+console.log(inputElement)
